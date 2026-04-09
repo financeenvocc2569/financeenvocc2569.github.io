db0b7443dd69e9588d5f80ad9b3dcdc38daa8ec0
finance2562envocc-debug <financeenvocc2569@gmail.com>
2026-04-09 10:35:31 +0700

    +

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.APP_STATIC_CONFIG = Object.assign({}, window.APP_STATIC_CONFIG || {}, {
-  apiBase: 'https://script.google.com/macros/s/AKfycbwhFStU1-4O3EyrC5y-TYWEdZI5FyH1rYKxn6PmtNugMnoA008-gqyYQFhsPd5_kc29/exec'
+  apiBase: 'https://script.google.com/macros/s/AKfycby1T9hpoepTr9rr5ht_zamjueSpBQYrUnvlY6xJQoQKoTTjMhkvYN5I6pxUHBXGrmdU/exec'
 });
